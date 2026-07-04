a1e79411815888142a17736952fa6fd9d080b1be
Rashmi G <229494151+Rashmi-G2011@users.noreply.github.com>
2026-07-04 17:19:06 +0000

    add basic python programs

---
+++ basic programs.py
@@ -0,0 +1,34 @@
+print("hello world!")
+name='Rashmi'
+age=20
+print("My name is", name, "and I am", age, "years old.")
+#area of rectangle
+l=5
+b=10
+area=l*b
+print("Area of rectangle is:", area)
+#celsius to fahrenheit
+celsius=65
+fahrenheit=(celsius*9/5)+32
+print(celsius, "degree Celsius is equal to", fahrenheit, "degree Fahrenheit.")
+#simple calculator
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+sum=num1+num2
+diff=num1-num2
+product=num1*num2
+quotient=num1/num2
+print("Sum:", sum)
+print("Difference:", diff)
+print("Product:", product)
+print("Quotient:", quotient)
+#percentage calculator
+marks_obtained=int(input("Enter marks obtained: "))
+total_marks=int(input("Enter total marks: "))
+percentage=(marks_obtained/total_marks)*100
+print("Percentage:", percentage, "%")
+#age calculator
+current_year=int(input("Enter current year: "))
+birth_year=int(input("Enter your birth year: "))
+age=current_year-birth_year
+print("Your age is:", age, "years old.")
